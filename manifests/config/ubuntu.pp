d66c7bd288f6e9e24778bacc3320858f314ca2d3
Geoff Garside <geoff@geoffgarside.co.uk>
2014-10-17 13:09:58 +0100

    Implement redis::config::ubuntu class
    Sets the vm.overcommit_memory sysctl in both /etc/sysctl.conf and live. Also sets the ULIMIT value for Redis to 10032, this is currently hard coded.

--- manifests/config/ubuntu.pp
+++ manifests/config/ubuntu.pp
@@ -1,4 +1,21 @@
 class redis::config::ubuntu {
-  # TODO: Set vm.overcommit_memory = 1 in /etc/sysctl.conf on Ubuntu
-  # TODO: Set ULIMIT=10032 in /etc/default/redis-server on Ubuntu
+  $ulimit = 10032 # TODO: Move to init.pp, needs to support other platforms though
+
+  file { '/etc/defaults/redis-server':
+    ensure  => $::redis::file_ensure,
+    content => template('redis/defaults_redis-server.erb'),
+    mode    => '0644',
+    owner   => 'root',
+    group   => '0',
+  }
+
+  augeas { '/etc/sysctl.conf vm.overcommit_memory = 1':
+    context => "/files/etc/sysctl.conf",
+    changes => "set vm.overcommit_memory 1",
+  }
+
+  exec { 'sysctl-set-vm.overcommit_memory=1':
+    command => "sysctl -w vm.overcommit_memory=1",
+    unless  => "sysctl vm.overcommit_memory | grep 'vm.overcommit_memory = 1'",
+  }
 }
