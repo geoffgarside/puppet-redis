5b743b927180fd9014c707818c0d1ee0c46de114
Geoff Garside <geoff@geoffgarside.co.uk>
2014-11-04 14:09:09 +0000

    Attempt to disable redis-server startup on Debian

--- manifests/install.pp
+++ manifests/install.pp
@@ -12,4 +12,23 @@ class redis::install {
     ensure => $package_ensure,
     name   => $::redis::params::package_name,
   }
+
+  if $::osfamily == 'Debian' {
+    # Disable redis-server startup after the package has been installed
+    # NOTE: I hate you Debian
+
+    exec { 'redis::package::apt_policy':
+      cwd     => '/tmp',
+      command => "echo \"#!/bin/sh\nexit 101\n\" > /usr/sbin/policy-rc.d ; chmod +x /usr/sbin/policy-rc.d",
+    }
+
+    exec { 'redis::package::remove_apt_policy':
+      command     => 'rm -f /usr/sbin/policy-rc.d',
+      refreshonly => true,
+    }
+
+    Exec['redis::package::apt_policy'] ->
+    Package['redis'] ~>
+    Exec['redis::package::remove_apt_policy']
+  }
 }
