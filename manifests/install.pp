5546f3c8dbb4faa55e3329f846180457b6a2e7d7
Geoff Garside <geoff@geoffgarside.co.uk>
2014-11-04 15:58:36 +0000

    Attempt to stop redis-server after initial install
    This should allow the redis user changes to be completed before the service is started properly.

--- manifests/install.pp
+++ manifests/install.pp
@@ -12,4 +12,14 @@ class redis::install {
     ensure => $package_ensure,
     name   => $::redis::params::package_name,
   }
+
+  if $::osfamily == 'Debian' {
+    # Stop redis after install to allow redis::config to succeed
+    exec { 'redis::package':
+      command     => '/etc/init.d/redis-server stop ; touch /tmp/._redis_package_installed',
+      creates     => '/tmp/._redis_package_installed',
+      refreshonly => true,
+      subscribe   => Package['redis'],
+    }
+  }
 }
