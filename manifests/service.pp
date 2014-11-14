61f2f1421fe0ed70337025202baf477bcbfb7940
Geoff Garside <geoff@geoffgarside.co.uk>
2014-11-14 23:12:25 +0000

    Add Darwin plist installation.

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,9 +1,11 @@
 class redis::service {
   case $::redis::ensure {
     'absent', absent: {
+      $file_ensure    = 'absent'
       $service_ensure = 'absent'
     }
     default: {
+      $file_ensure    = 'file'
       $service_ensure = 'running'
     }
   }
@@ -14,5 +16,18 @@ class redis::service {
     hasstatus  => true,
     hasrestart => true,
     name       => $::redis::params::service_name,
+  }
+
+  if $::osfamily == 'Darwin' and $::has_brew {
+    # ln -sfv /usr/local/opt/redis/*.plist ~/Library/LaunchAgents
+    file { "/Library/LaunchDaemons/${::redis::params::service_name}.plist":
+      ensure => $file_ensure,
+      owner  => 'root',
+      group  => '0',
+      mode   => '0644'
+      source => '/usr/local/opt/redis/${::redis::params::service_name}.plist',
+      before => Service['redis'],
+      notify => Service['redis'],
+    }
   }
 }
